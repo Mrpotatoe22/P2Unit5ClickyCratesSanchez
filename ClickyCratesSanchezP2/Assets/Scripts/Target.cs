a2f8442b84dc57546aff1d76b738e6bb1f7cd873
Mrpotatoe22 <726318@student.dusd.net>
2025-05-20 09:57:11 -0700

    Finished Main part
    Working on difficulty

--- ClickyCratesSanchezP2/Assets/Scripts/Target.cs
+++ ClickyCratesSanchezP2/Assets/Scripts/Target.cs
@@ -10,7 +10,10 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -2;
+    private float ySpawnPos = -6;
+
+    public ParticleSystem explosionParticle;
+    public int pointValue;
 
     // Start is called before the first frame update
     void Start()
@@ -33,13 +36,23 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+        gameManager.GameOver();
     }
 
     Vector3 RandomForce()
